a7ba59dc223ae66c2d7495f7acfb9c0041336f0c
D Pinto <denispmaciel@gmail.com>
2016-07-07 16:08:28 +0200

    Added Figure 1b and changed the R code for generating the shocks to get the directly from R (without using matlab)

--- FormatShocks(R Code).R
+++ FormatShocks(R Code).R
@@ -3,14 +3,33 @@ setwd("/Users/denismaciel/Documents/fiscalfreelunch")
 
 df <- read.csv("denis.csv")
 
+dfseq(0,50, 0.1)
+
+
+
 j <- c()
-for (i in 1:nrow(df)) {
-  j <- paste0(j, ",","'",  df[i,] ,"'")
+for (i in 1:length(df)) {
+  j <- paste0(j, ",","'",  df[i] ,"'")
 }
 j
 
 k <- c()
-for (i in 1:nrow(df)) {
-  k <- paste0(k, ",",  df[i,])
+for (i in 1:length(df)) {
+  k <- paste0(k, ",",  df[i])
 }
 k
+
+
+
+
+# j <- c()
+# for (i in 1:nrow(df)) {
+#   j <- paste0(j, ",","'",  df[i,] ,"'")
+# }
+# j
+
+# k <- c()
+# for (i in 1:nrow(df)) {
+#   k <- paste0(k, ",",  df[i,])
+# }
+# k
